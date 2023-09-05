35753900dd5b9a408257d82f1d4aebb4b693ba1c
Raja <rajawaheed5473238@gmail.com>
2023-09-05 18:20:40 +0500

    literals

---
+++ literals.js
@@ -0,0 +1,6 @@
+let Uniontype;
+let literaltype;
+let a;
+a = "hello world";
+console.log(a);
+export {};
---
+++ literals.ts
@@ -0,0 +1,8 @@
+let Uniontype: string|number;
+let literaltype: "sultan"|232;
+//when we use literaltype we can assign value than type
+type World = "world";
+type greeting = `hello ${World}`
+let a:greeting
+a= "hello world"
+console.log(a);
