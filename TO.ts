f3df6fea61fd976f55ca483a0e9023101f880113
Raja <rajawaheed5473238@gmail.com>
2023-09-05 18:10:37 +0500

    ternary operators

---
+++ TO.ts
@@ -0,0 +1,21 @@
+//it is like if,else but with a slight difference
+//syntax
+//condition or conditions ? code1 : code2 (code1 for true and code2 for false)
+let username = "bilal";
+
+// if (username== "bilal"){
+//     console.log("loggedin");
+    
+// }else{
+//     console.log("Invalid username");
+    
+// }
+
+
+//now ternary.O
+// username == "bilal" 
+// ? console.log("loggedin")
+// : console.log("Invalid username")
+
+
+
